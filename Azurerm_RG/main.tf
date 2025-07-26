bbed1fcb783c742158c012f6bdb3d25c2293e811
Avnesh <avneshkushwaha@gmail.com>
2025-07-26 14:55:21 +0530

    story

--- Azurerm_RG/main.tf
+++ Azurerm_RG/main.tf
@@ -1,6 +1,6 @@
 This Terraform configuration creates an Azure Resource Group. 
-
-
+ssdfsfd
+dssd
 resource "azurerm_resource_group" "resource_group" {
   name     = var.RG_name
   location = var.location
