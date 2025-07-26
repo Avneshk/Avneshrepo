f4f6eac3c43cbf107e7e0bd9ce0545455a1aea70
Avnesh <avneshkushwaha@gmail.com>
2025-07-26 15:04:09 +0530

    new Avnesh story

--- Azurerm_RG/variable.tf
+++ Azurerm_RG/variable.tf
@@ -6,3 +6,9 @@ variable "RG_name" {
 variable "location" {
   type        = string  
 }
+
+
+
+copilot "This Terraform configuration creates an Azure Resource Group." {
+  description = "This variable defines the name of the Azure Resource Group."
+}
